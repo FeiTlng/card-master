0245b23834300c97d7592f4fd520f627b5231003
yanjjingdong <1215524238@qq.com>
2023-06-17 04:00:27 +0800

    Test code

--- cardmaster/src/main/java/top/kuronoks/App.java
+++ cardmaster/src/main/java/top/kuronoks/App.java
@@ -1,9 +1,21 @@
 package top.kuronoks;
 
+
+import lombok.extern.slf4j.Slf4j;
 import top.kuronoks.core.server.CMServer;
 
-public class App {
-    public static void main(String[] args) {
+@Slf4j
+public class App
+{
+    public static void main( String[] args )
+    {
+        log.info("123");
+        log.info("888");
+        log.debug("debug");
+        log.error("error");
+        log.warn("warn");
+        log.trace("trace");
+
         CMServer cmServer = new CMServer();
         cmServer.bind();
     }
